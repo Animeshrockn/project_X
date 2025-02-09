e946e82792b412c6a3bae96d76bde0d605dc4238
Animesh <animeshrockn@gmail.com>
2025-02-09 10:34:20 -0800

    Added a new header component

---
+++ frontend/src/app/components/animesh-header/animesh-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animesh-header',
+  imports: [],
+  templateUrl: './animesh-header.component.html',
+  styleUrl: './animesh-header.component.css'
+})
+export class AnimeshHeaderComponent {
+
+}
