4d4c1abe466718f2e08e1229940645693c446607
Aniket <aniketmahajan20@gmail.com>
2025-03-16 00:46:37 -0700

    added changes to fetch the menu items from database

--- backend/app.py
+++ backend/app.py
@@ -11,13 +11,14 @@ CORS(app)
 
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://Cluster16349:a1l0RVViYVNu@cluster16349.0gftb.mongodb.net/")
-db = client['nikita']
-collection = db['nikita_test']
+db = client['project_x']
+menu_items_collection = db['menu_items']
+
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     print("Hello")
-    data = list(collection.find())  # Exclude MongoDB _id
+    data = list(menu_items_collection.find())  # Exclude MongoDB _id
     document_json = json.dumps(data, default=json_util.default)
     print(data)
     return document_json
@@ -25,41 +26,15 @@ def get_data():
 @app.route('/api/data', methods=['POST'])
 def add_data():
     data = request.json
-    collection.insert_one(data)
+    menu_items_collection.insert_one(data)
     return jsonify({"message": "Data added successfully!"}), 201
 
 @app.route('/api/menu_items', methods=['GET'])
 def get_menu_items():
     # This data will go into the database
-    menuItems = [{
-        "itemId": 0,
-        "available": True,
-        "itemName": "Daal Makhni",
-        "itemDescription": "A Tasty blend of black lentils and Butter, A must have for family meals",
-        "cost": 19
-    },
-    {
-        "itemId": 1,
-        "available": True,
-        "itemName": "Chicken Vindaloo",
-        "itemDescription": "Known for its spicy and tangy flavor combination",
-        "cost": 20
-    },
-    {
-        "itemId": 2,
-        "available": True,
-        "itemName": "BURRATA",
-        "itemDescription": "roasted beets, shiso olive pesto, toasted hazelnut, pickled beets, sweet beet powder, basil oil, crispy roots, chinese donut",
-        "cost": 29
-    },
-    {
-        "itemId": 3,
-        "available": True,
-        "itemName": "Lachcha Paratha",
-        "itemDescription": "This legacy naan recipe from India whih is crunchy and soft at the same time",
-        "cost": 4
-    }
-    ]
-    return menuItems
+    data = list(menu_items_collection.find())
+    menu_items = json.dumps(data, default=json_util.default)
+    return menu_items
+
 if __name__ == '__main__':
     app.run(debug=True)
